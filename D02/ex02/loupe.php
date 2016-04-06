7a40f354b2b497df03934720399775014a27aa9c
Visentin <tvisenti@42.student.fr>
2016-04-06 21:06:32 +0200

    Work in progress

---
+++ D02/ex02/loupe.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+    preg_replace_callback("/title="(.*?)"/", );
+    preg_replace_callback("/http:.*?>\s*(.*?)\s*</", );
+    preg_replace_callback("/(.*?)\s*<.*?>/", );
+
+    preg_grep()
+?>
+
+
+title="(.*?)"
+http:.*?>\s*(.*?)\s*<
+(.*?)\s*<.*?>
+
+MUST HAVE AFTER
+<a.*?>(.*?)<\/a>
+(.*?)\s*<.*?>
